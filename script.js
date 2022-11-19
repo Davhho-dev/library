4330b20a6ebd9622a63af5aabfca9c696551fc16
Davhho-dev <Davhho@utexas.edu>
2022-11-19 10:03:47 -0600

    Create Book object, add book to library array and create card with book object values

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const modal = document.querySelector(".modal");
 const addBook = document.querySelector(".add-book");
 const submit = document.querySelector(".submit");
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const pages = document.getElementById("pages");
+const ifRead = document.getElementById("read");
+
 
 submit.addEventListener("click", (e) => {
     e.preventDefault();
+    // console.log(`title: ${title.value}, author: ${author.value}, pages: ${pages.value}, read: ${ifRead.value}`);
+    const newBook = new Book(title.value, author.value, pages.value, ifRead.value);
+    addBookToLibrary(newBook);
 })
+
 
 addBook.addEventListener("click", () => {
     modal.showModal();
@@ -19,3 +28,35 @@ document.addEventListener("click", (e) => {
         }, {once: true});
     }
 })
+
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary(bookObj) {
+    myLibrary.push(bookObj);
+    console.log(myLibrary);
+    createBookCard(bookObj);
+}
+
+function createBookCard(bookObj) {
+    const cardsContainer = document.querySelector(".cards-container");
+    const card = document.createElement("div");
+    card.classList.add("card");
+    const title = document.createElement("h2");
+    title.classList.add("title");
+    const author = document.createElement("p");
+    author.classList.add("author");
+    const pages = document.createElement("p");
+    pages.classList.add("pages");
+    title.innerText = `${bookObj.title}`;
+    author.innerText = `${bookObj.author}`;
+    pages.innerText = `${bookObj.pages}`;
+    card.append(title, author, pages);
+    cardsContainer.append(card);
+}
